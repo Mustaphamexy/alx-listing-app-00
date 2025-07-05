044da998d8dad1c53d1895c5c74000cccb85a871
Mustaphamexy <mustaphamexy111@gmail.com>
2025-07-05 21:27:16 +0100

    first commit again

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+// constants/index.ts
+
+export const DEFAULT_BUTTON_LABEL = 'Click me';
+export const DEFAULT_CARD_TITLE = 'Card Title';
+export const DEFAULT_CARD_DESCRIPTION = 'Card description goes here.';
