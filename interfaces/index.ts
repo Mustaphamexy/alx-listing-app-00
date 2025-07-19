321b01a96d90e2612454ff3008aadc6ff1bd2f2a
Mustaphamexy <mustaphamexy111@gmail.com>
2025-07-19 12:41:37 +0100

    Header And Footer update

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -14,3 +14,28 @@ export interface ButtonProps {
   className?: string;
   type?: 'button' | 'submit' | 'reset';
 }
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants:string;
+  };
+  image: string;
+  discount: string;
+}
+
+export interface AccommodationType {
+  id: string;
+  name: string;
+  icon: React.ReactNode;
+}
